e5b1609304ef9c43318fe99dffa35764cc3a13b9
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-11-14 21:57:53 +0700

    add "Tambah" button

--- resources/views/pages/hewan.blade.php
+++ resources/views/pages/hewan.blade.php
@@ -8,8 +8,11 @@
 <div class="card-deck mb-5">
 	@foreach ($listhewan as $hewan)
 	<div class="card">
-		<img class="card-img-top" src="{{ asset("storage/{$hewan->satugambarhewan->path}") }}"
-			style="max-width: 100%;height: auto" alt=":v">
+	@if($hewan->satugambarhewan == null)
+	wkwkwk --> {{ Auth::user()->admin }}
+	@else
+		<img class="card-img-top" src="{{ asset('storage') }}/{{$hewan->satugambarhewan}}" style="max-width: 100%;height: auto" alt=":v">
+	@endif
 		<div class="card-body">
 			<h5 class="card-title">{{ $hewan->nama }}</h5>
 			<h6 class="card-text">Rp{{ $hewan->harga }}</h6>
@@ -21,5 +24,8 @@
 {{ $listhewan->links() }}
 @else
 <p>Tidak ada hewan tersedia :(</p>
+@endif
+@if ( Auth::user()->admin == 0 )
+<a class="btn btn-primary" href="{{ url("/hewan/$hewan->id") }}">Tambah</a>
 @endif
 @endsection
