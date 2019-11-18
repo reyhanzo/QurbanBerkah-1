eee29ca1e0124904fee5f1fed9a128a00f1f3fe2
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-11-18 16:05:43 +0700

    change back button position

--- resources/views/pages/tambahhewan.blade.php
+++ resources/views/pages/tambahhewan.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
 <div class="mb-3">
-	<h1>Tambah hewan</h1>
-</div>
-<div class="mb-3">
 	<a class="btn btn-danger" href="{{ url("/hewan") }}">Kembali</a>
+</div>
+
+<div class="mb-3">
+	<h1>Tambah hewan</h1>
 </div>
 @endsection
