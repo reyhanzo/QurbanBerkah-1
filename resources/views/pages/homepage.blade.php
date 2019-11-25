53837d54137b31ecb090b87b2938cc206e307e41
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-11-25 14:46:09 +0700

    add "Lihat Hewan" button

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,5 +5,7 @@
 	<div class="jumbotron text-center">
 		<h1>Selamat Datang di<br>QurbanBerkah</h1>
 	</div>
+
+	<a class="btn btn-primary" href="{{ url("/hewan") }}">Lihat Hewan</a>
 </div>
 @endsection
