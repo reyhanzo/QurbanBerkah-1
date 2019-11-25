c480dc18157709e2c1ee0145b57427d1f339d3c1
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-11-25 13:33:06 +0700

    add "Hapus" button, but no effect

---
+++ resources/views/pages/edithewan.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="mb-3">
+		<a class="btn btn-primary" href="{{ url("/hewan") }}">Kembali</a>
+	</div>
+
+    
+</div>
